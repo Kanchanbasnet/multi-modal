5211677a4ff3029f66164ce1f77c1e2434db4187
Kanchan Basnet <kanchanbasnet.dev@gmail.com>
2026-05-02 00:20:20 +0545

    fix: lint issues

--- apps/api/src/middleware/errorHandler.ts
+++ apps/api/src/middleware/errorHandler.ts
@@ -4,6 +4,7 @@ export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   _next: NextFunction,
 ): void => {
   req.log.error({ err }, 'Unhandled Error');
